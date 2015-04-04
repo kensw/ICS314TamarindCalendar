4795921eeb29b00aad449ed33661b58b87e68b0c
kensw <kensw@hawaii.edu>
2015-04-03 23:40:45 -1000

    First Iteration Test

--- CalendarTest.java
+++ CalendarTest.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.io.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -6,8 +8,20 @@ import org.junit.Test;
 public class CalendarTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void teststartUp() {
+		
+		String data = "3\n";
+		InputStream stdin = System.in;
+		try {
+		  System.setIn(new ByteArrayInputStream(data.getBytes()));
+		  Scanner scanner = new Scanner(System.in);
+		  System.out.println(scanner.nextLine());
+		} finally {
+		  System.setIn(stdin);
+		}
+		
+		assertNotNull("Testing Startup: NULL?", startUp());
+
 	}
 
 }
